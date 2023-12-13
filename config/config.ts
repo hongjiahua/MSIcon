887c28264b3f37ae7de2821d252a368695e81998
HONGJIAHUA <www.2640601916@outlook.com>
2023-12-13 11:19:19 +0800

    1

--- config/config.ts
+++ config/config.ts
@@ -79,7 +79,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: 'MSIcon',
+  title: 'MSIcon12',
   layout: {
     locale: true,
     ...defaultSettings,
